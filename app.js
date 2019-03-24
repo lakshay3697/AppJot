91c36bd01a8595e9eb8ef24c7d2f29d7784f5b29
lakshay3697 <lakshaygbpec@gmail.com>
2019-03-24 17:17:57 +0530

    Authentication using passportjs & Login module done

--- app.js
+++ app.js
@@ -6,12 +6,14 @@ const bodyParser = require('body-parser');
 const methodOverride = require('method-override');
 const flash = require('connect-flash');
 const session= require('express-session');
-
+const passport = require('passport');
 
 const app = express();
 
 const ideas = require('./routes/ideas');
 const users = require('./routes/users');
+
+require('./config/passport')(passport);
 
 mongoose.Promise = global.Promise;
 
@@ -39,12 +41,17 @@ app.use(session({
     saveUninitialized : true,
 }));
 
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use(flash());
 
+// Global variables
 app.use(function(req,res,next){
     res.locals.success_msg = req.flash('success_msg');
     res.locals.error_msg = req.flash('error_msg');
     res.locals.error = req.flash('error');
+    res.locals.user = req.user || null;
     next();
 });
 
--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
+const passport = require('passport');
 
 require('../models/User');
 const User = mongoose.model('users');
@@ -12,6 +13,14 @@ router.get('/login',(req,res) => {
 
 router.get('/register',(req,res) => {
     res.render('users/register');
+});
+
+router.post('/login',(req,res,next) => {
+    passport.authenticate('local',{
+        successRedirect : '/ideas',
+        failureRedirect : '/users/login',
+        failureFlash : true
+    })(req,res,next);
 });
 
 router.post('/register',(req,res) => {
